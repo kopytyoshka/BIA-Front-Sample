8f2dedc56cf450bfdd50f9a181523deeb2af198d
kopytyoshka <ilyatttt19@gmail.com>
2023-06-21 18:27:50 +0300

    feat: check cookies userId and get it

--- src/scripts/MainPageLoader.tsx
+++ src/scripts/MainPageLoader.tsx
@@ -6,8 +6,36 @@ import Registration from "../pages/login-pages/Registration";
 
 function MainPageByRole() {
     const [role, setRole] = useState("")
+    const [userId, SetUserId] = useState("")
+    useEffect(() => {
+        const getUserIdFromCookie = (): string => {
+            const name = 'userId=';
+            const decodedCookie = decodeURIComponent(document.cookie);
+            const cookieArray = decodedCookie.split(';');
+
+            for (let i = 0; i < cookieArray.length; i++) {
+                let cookie = cookieArray[i];
+                while (cookie.charAt(0) === ' ') {
+                    cookie = cookie.substring(1);
+                }
+                if (cookie.indexOf(name) === 0) {
+                    return cookie.substring(name.length, cookie.length);
+                }
+            }
+
+            return "Error to get UserId";
+        };
+
+        // Get the user ID from the cookie
+        const userId = getUserIdFromCookie();
+        SetUserId(userId)
+
+        // Use the user ID as needed
+        console.log(userId);
+    }, []);
+
     const fetchData = () => {
-        fetch("/api/userInfo/getUserInformation/")
+        fetch("/api/userInfo/getUserInformation?userId=" + userId)
             .then(response => {
                 return response.json()
             })
